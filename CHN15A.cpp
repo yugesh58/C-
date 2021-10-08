99355336149bdef2d7916b0c31ae9c8492de7cd4
Yugesh bhadwal <73513064+yugesh58@users.noreply.github.com>
2021-10-08 22:02:58 +0530

    Add files via upload
    https://www.codechef.com/problems/CHN15A

---
+++ CHN15A.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+int t;
+cin>>t;
+while(t--){
+    int n,k,count=0;
+    cin>>n>>k;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+        a[i]=a[i]+k;
+    }
+    for(int i=0;i<n;i++){
+        if(a[i]%7==0){
+            count++;
+        }
+    }
+    cout<<count<<endl;
+}
+
+return 0;
+}
