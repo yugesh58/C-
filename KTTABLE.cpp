28c05af6c68ff424590987f846bf0560a152ed5c
Yugesh bhadwal <73513064+yugesh58@users.noreply.github.com>
2021-10-13 23:39:25 +0530

    Add files via upload
    https://www.codechef.com/problems/KTTABLE

---
+++ KTTABLE.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+int t;
+cin>>t;
+while(t--){
+    int n;
+    cin>>n;
+    int a[n],b[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    } 
+    for(int i=0;i<n;i++){
+        cin>>b[i];
+    } 
+    int count=0;
+    if(a[0]>=b[0])count+=1;
+    for(int i=1;i<n;i++){
+        if((a[i]-a[i-1])>=b[i]){
+            count++;
+    }
+    }
+    cout<<count<<endl;
+}
+
+return 0;
+}
